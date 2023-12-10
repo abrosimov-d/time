b78fc4c2b7b74dd8a8a55b6cea1f9b707cf4f95d
abrosimov-d <escape_from_azer0th@proton.me>
2023-12-10 20:03:32 +0300

    add: Clock class

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 class Clock {
-    constructor(canvasElement) {
-        this.canvas = canvasElement;
-        this.context = this.canvas.getContext('2d');
+	constructor(canvasElement) {
+		this.canvas = canvasElement;
+		this.context = this.canvas.getContext('2d');
+		this.width = this.canvas.width;
 
-    }
+		this.delta = [
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+			[0, 1, 0, 0]];
+	}
 
-    draw() {
-        this.context.fillStyle = 'red';
-        this.context.fillRect(0, 0, 100, 10);
-    }
+	drawElem(pos, index) {
+
+		let width = this.width / 10;
+		let height = this.width / 60;
+		let x = pos * width + height * pos;
+		let y = (this.delta[index][1]) * height;
+		this.context.fillRect(x, y, width, height);
+
+		console.log(width, height, x, y, this.delta[1][1])
+	}
+
+	draw() {
+		for (let i = 0; i < 4; i++) {
+			this.drawElem(i, i);
+		}
+	}
 }
 
-
 class App {
-    constructor() {
-        this.canvas = document.querySelector('.canvas');
-        this.clock = new Clock(this.canvas);
-    }
+	constructor() {
+		this.canvas = document.querySelector('.canvas');
+		this.clock = new Clock(this.canvas);
+	}
 
-    draw() {
-        this.clock.draw();
-    }
+	draw() {
+		this.clock.draw();
+	}
 
-    render() {
+	render() {
 
-    }
+	}
 }
 
 window.onload = () => {
-    let app = new App();
-    app.render();
-    app.draw();
+	let app = new App();
+	app.render();
+	app.draw();
 }
